3118a61d50db3d24e5685046189ef1efe8e1f722
GUNJESH KUMAR <gunjesh843@gmail.com>
2024-11-27 00:30:50 +0530

    forgot password

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -12,6 +13,12 @@ const userSchema = new mongoose.Schema({
   otpExpiry: Date,
 });
 
+// Hash the password before saving the user
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
